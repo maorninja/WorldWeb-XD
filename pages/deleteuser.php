c8dae68a261e9d7a17802bdcba9887b83ca7ef09
EzioisAwesome56 <jedijon567@gmail.com>
2017-04-05 17:32:45 -0400

    lolfail, it was already there
    dont be a moron, jon!

--- pages/deleteuser.php
+++ pages/deleteuser.php
@@ -99,8 +99,6 @@ if ($userdeleteperms && $user["primarygroup"] < 1 && $user["primarygroup"] > 0 &
 			query("delete from {users}
 					where id={0}", $uid);
 			
-			//Log it
-			Report("[b]".$user["name"]."[/] was deleted from the database", 1);
 
 			//and then IP BAN HIM
 			query("insert into {ipbans} (ip, reason, date) 
